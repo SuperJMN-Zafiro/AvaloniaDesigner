48618c6fe2f90900ca588f20fcd5f8b426051417
José Manuel Nieto <superjmn@outlook.com>
2019-12-13 23:34:32 +0100

    Tools (#3)
    * Added Tools
    
    * Improved layout
    Line Control
    
    * Updated Designer
    
    * Updated Designer
    
    * Shared Core
    
    * Removed SuppaDesigner

---
+++ AvaloniaDesigner/Parts/Graphics/LineControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDesigner.Parts.Graphics
+{
+    public class LineControl : UserControl
+    {
+        public LineControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
