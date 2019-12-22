6339cf9099ac424e198e065001a0075acd0cacdc
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-12-22 17:59:30 +0100

    ColorPicker

---
+++ AvaloniaDesigner/Converters/ModelColorToColorConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using ModelColor = Designer.Domain.Models.Color;
+
+namespace AvaloniaDesigner.Converters
+{
+    public class ModelColorToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ModelColor p)
+            {
+                return new Color(p.A, p.R, p.G, p.B);
+            }
+
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color c)
+            {
+                return ModelColor.FromArgb(c.A, c.R, c.G, c.B);
+            }
+
+            return null;
+        }
+    }
+}
